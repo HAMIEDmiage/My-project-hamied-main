a3663c43ae6305591ef2111cb8c458e8471775f4
HAMIEDmiage <Hamiedwalied1978@gmail.com>
2024-11-13 14:22:21 +0100

    Initial commit

---
+++ Assets/Scripts/Camerasuivi.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Camerasuivi : MonoBehaviour
+{
+    public GameObject Player;
+    public float timeOffset;
+    public Vector3 posOffset;
+    private Vector3 velocity;
+
+        void Update()
+    {
+     transform.position = Vector3.SmoothDamp(transform.position, Player.transform.position + posOffset, ref velocity, timeOffset);   
+    }
+}
